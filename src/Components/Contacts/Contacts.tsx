72c373470eeb38142f567ca32033487f95d5a759
VilenaArturovna <vilena-forever@inbox.ru>
2021-05-26 17:15:09 +0600

    added feedback form

--- src/Components/Contacts/Contacts.tsx
+++ src/Components/Contacts/Contacts.tsx
@@ -2,21 +2,90 @@ import React from "react";
 import style from './Contacts.module.scss'
 import styleContainer from "../../Common/Styles/Container.module.scss";
 import {Title} from "../../Common/Components/Title/Title";
+import {Formik} from 'formik';
+import axios from "axios";
+
+export type FormikType = {
+    email: string
+    name: string
+    theme: string
+    message: string
+}
+
+type FormikErrorType = {
+    email?: string
+    name?: string
+    theme?: string
+    message?: string
+}
 
 export function Contacts() {
     return (
         <section className={style.contactsBlock}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
                 <Title title={'Контакты'} id={'contacts'}/>
-                <form className={style.formContainer}>
-                    <div>
-                        <div className={style.paddingForm}><input type="text" placeholder={'Ваше имя'}/></div>
-                        <div className={style.paddingForm}><input type="email" placeholder={'Ваш e-mail'}/></div>
-                    </div>
-                    <div className={style.paddingForm}><input type="text" placeholder={'Тема сообщения'}/></div>
-                    <div className={style.paddingForm}><textarea placeholder={'Сообщение'}/></div>
-                    <button type={'submit'}>Отправить</button>
-                </form>
+                <Formik
+                    initialValues ={{email: '', name: '', theme: '', message: ''}}
+                    validate={values => {
+                        const errors: FormikErrorType = {};
+                        if (!values.email) {
+                            errors.email = 'Required';
+                        } else if (
+                            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                        ) {
+                            errors.email = 'Invalid email address';
+                        }
+                        return errors;
+                    }}
+                    onSubmit={(values, {setSubmitting}) => {
+                        axios.post('http://localhost:3010/sendMessage', {values})
+                            .then(() => alert('Your message has been send'))
+                        setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2));
+                            setSubmitting(false);
+                        }, 400);
+                    }}
+                >
+                    {({
+                          values,
+                          errors,
+                          touched,
+                          handleChange,
+                          handleBlur,
+                          handleSubmit,
+                          isSubmitting,
+                          /* and other goodies */
+                      }) => (
+                        <form onSubmit={handleSubmit} className={style.formContainer}>
+
+                            <div>
+                                <div className={style.paddingForm}><input type="text" name="name"
+                                                                          onChange={handleChange}
+                                                                          onBlur={handleBlur}
+                                                                          value={values.name} placeholder={'Ваше имя'}/>
+                                </div>
+                                <div className={style.paddingForm}><input type="email" name="email"
+                                                                          onChange={handleChange}
+                                                                          onBlur={handleBlur}
+                                                                          value={values.email}
+                                                                          placeholder={'Ваш e-mail'}/>
+                                {errors.email && touched.email && errors.email}</div>
+                            </div>
+                            <div className={style.paddingForm}><input type="text" name="theme"
+                                                                      onChange={handleChange}
+                                                                      onBlur={handleBlur}
+                                                                      value={values.theme}
+                                                                      placeholder={'Тема сообщения'}/></div>
+                            <div className={style.paddingForm}><textarea name="message"
+                                                                         onChange={handleChange}
+                                                                         onBlur={handleBlur}
+                                                                         value={values.message}
+                                                                         placeholder={'Сообщение'}/></div>
+                            <button type={'submit'} disabled={isSubmitting}>Отправить</button>
+
+                        </form>
+                    )}
+                </Formik>
             </div>
         </section>
     )
--- src/Components/Contacts/nodemailer.js
+++ src/Components/Contacts/nodemailer.js
@@ -2,11 +2,52 @@
 const express = require("express");
 // создаем объект приложения
 const app = express();
-// определяем обработчик для маршрута "/"
-app.get("/", function(request, response){
+// cors
+const cors = require('cors')
 
-    // отправляем ответ
-    response.send("<h2>Привет Express!</h2>");
+const nodemailer = require("nodemailer");
+
+const bodyParser = require('body-parser')
+
+app.use(cors())
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'vilena27arturovna@gmail.com',
+        pass: 'Integrirovanie27',
+    },
 });
-// начинаем прослушивать подключения на 3000 порту
-app.listen(3010);
+
+app.post('/sendMessage', async function (req, res) {
+
+    const {values} = req.body
+
+    // send mail with defined transport object
+   await transporter.sendMail({
+        from: 'Vilena Portfolio Feedback',
+        to: "vilena-forever@inbox.ru",
+        subject: "HR wants me!!",
+        html: `<div>
+                    <div>Name: ${values.name}</div>
+                    <div>E-mail: ${values.email}</div>
+                    <div>Theme: ${values.theme}</div>
+                    <div>Message: ${values.message}</div>
+               </div>`,
+    });
+
+})
+
+app.listen(3010, function () {
+    console.log('Example app listening on port 3010')
+})
+
+
+
